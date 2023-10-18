59f53df189b9afb8a00e2638f4571ef4c27b616b
Daniel Hally <2660097h@student.gla.ac.uk>
2023-10-18 21:37:42 +0100

    Initial models draft
    Creates all the models that should be needed for the search and game view pages

--- neuro_arcade/na/admin.py
+++ neuro_arcade/na/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from na.models import GameTag, Game, Player, AITag, AI, ScoreType, Score
+
 # Register your models here.
+
+admin.site.register(GameTag)
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(AITag)
+admin.site.register(AI)
+admin.site.register(ScoreType)
+admin.site.register(Score)
--- neuro_arcade/na/models.py
+++ neuro_arcade/na/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
 # Create your models here.
+
+class GameTag(models.Model):
+    """Category for a game"""
+
+    MAX_NAME_LENGTH = 64
+    MAX_DESCRIPTION_LENGTH = 1024
+
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+    description = models.TextField(max_length=MAX_DESCRIPTION_LENGTH)
+
+
+class Game(models.Model):
+    """Description of a game added to the website"""
+
+    MAX_NAME_LENGTH = 64
+    MAX_DESCRIPTION_LENGTH = 1024
+
+    MEDIA_SUBDIR = 'game_icons'
+
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+    description = models.TextField(max_length=MAX_DESCRIPTION_LENGTH)
+    icon = models.ImageField(upload_to=MEDIA_SUBDIR)
+    tags = models.ManyToManyField(GameTag)
+
+
+class Player(models.Model):
+    """An entity that can appear on a scoreboard, human or AI
+    
+    AI players will also have an AI instance linked to it"""
+
+    MAX_NAME_LENGTH = 64
+
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+
+
+class AITag(models.Model):
+    """A category of AI"""
+
+    MAX_NAME_LENGTH = 64
+    MAX_DESCRIPTION_LENGTH = 1024
+
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+    description = models.TextField(max_length=MAX_DESCRIPTION_LENGTH)
+
+
+class AI(models.Model):
+    """An AI model being evaluated by games on the site"""
+
+    MAX_DESCRIPTION_LENGTH = 1024
+
+    player = models.OneToOneField(Player, on_delete=models.CASCADE) # TODO: on_delete
+    description = models.TextField(max_length=MAX_DESCRIPTION_LENGTH)
+
+
+class ScoreType(models.Model):
+    """A category of score for a game"""
+
+    MAX_NAME_LENGTH = 64
+    MAX_DESCRIPTION_LENGTH = 1024
+
+    MEDIA_SUBDIR = 'evaluation_functions'
+
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+    description = models.TextField(max_length=MAX_DESCRIPTION_LENGTH)
+    code = models.FileField(upload_to=MEDIA_SUBDIR)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+
+class Score(models.Model):
+    """A result of a player playing a game"""
+
+    t = models.ForeignKey(ScoreType, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    value = models.IntegerField() # TODO: does this need to be a float?
+
