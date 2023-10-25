5614acb2b9278572df078af091f3aff257a2d046
Daniel Hally <2660097h@student.gla.ac.uk>
2023-10-25 09:36:22 +0100

    Add basic views for each url
    Sets up all currently known pages with a simple text response.
    Since work on the search page and game view page will happen in parallel, this
    completes the basic django setup that would be depended on by both.

--- neuro_arcade/na/views.py
+++ neuro_arcade/na/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from na.models import AI, Game
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Index page.")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("About page.")
+
+
+def game_search(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Game search page.")
+
+
+def game_view(request: HttpRequest, game_name_slug: str) -> HttpResponse:
+    game = get_object_or_404(Game, slug=game_name_slug)
+    return HttpResponse("Game view page.")
+
+
+def game_data_add(request: HttpRequest, game_name_slug: str) -> HttpResponse:
+    game = get_object_or_404(Game, slug=game_name_slug)
+    return HttpResponse("Game data add page.")
+
+
+def game_add(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Game add page.")
+
+
+def model_view(request: HttpRequest, model_name_slug: str) -> HttpResponse:
+    model = get_object_or_404(AI, slug=model_name_slug)
+    return HttpResponse("Model view page")
+
+
+def model_add(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Model add page")
--- neuro_arcade/neuro_arcade/urls.py
+++ neuro_arcade/neuro_arcade/urls.py
@@ -9,15 +9,19 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URLconf   
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.contrib.staticfiles.urls import static
 
+from na import views
+
+
 urlpatterns = [
+    path('', include('na.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
