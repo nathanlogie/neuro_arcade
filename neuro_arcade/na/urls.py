836b4d8c6b0489ea0a3d697ab051d0e0d0517de6
andrei <andrei.ghita2002@gmail.com>
2023-11-08 01:43:13 +0000

    na/views.py: changed views to work with the new database changes

--- neuro_arcade/na/urls.py
+++ neuro_arcade/na/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('games/<slug:game_name_slug>/', views.game_view, name='game_view'),
     path('games/<slug:game_name_slug>/add_data/', views.game_data_add, name='game_data_add'),
     path('add_game/', views.game_add, name='game_add'),
-    path('models/<slug:model_name_slug>/', views.model_view, name='model_view'),
+    path('models/<slug:model_name_slug>/', views.player_view, name='model_view'),
     path('add_model/', views.model_add, name='model_add'),
 ]
