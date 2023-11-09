ad24e016d45ca735344f89adb7cdb93f4584addb
Daniel Hally <2660097h@student.gla.ac.uk>
2023-11-09 15:05:34 +0000

    Basic login system
    Adds the backend side of the login system, and very basic pages and navbar buttons for signing up, logging in and
    logging out. Closes #56.

--- neuro_arcade/na/urls.py
+++ neuro_arcade/na/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('add_game/', views.game_add, name='game_add'),
     path('models/<slug:model_name_slug>/', views.player_view, name='model_view'),
     path('add_model/', views.model_add, name='model_add'),
+    path('logout/', views.logout, name='logout'),
+    path('login/', views.login, name='login'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
--- neuro_arcade/na/views.py
+++ neuro_arcade/na/views.py
@@ -1,7 +1,11 @@
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
 from na.models import Game, GameTag, Player
+from na.forms import UserForm
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -51,3 +55,66 @@ def player_view(request: HttpRequest, player_name_slug: str) -> HttpResponse:
 
 def model_add(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Model add page")
+
+
+def sign_up(request: HttpRequest) -> HttpResponse:
+    # Check not already logged in
+    if request.user.is_authenticated:
+        return redirect(reverse('na:index'))
+
+    # Check if submitting or loading
+    if request.method == 'POST':
+        # Validate submission
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            # Update database
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            # Send user to login page
+            return redirect(reverse('na:login'))
+    else:
+        # Setup empty form
+        user_form = UserForm()
+
+    context_dict = {
+        'user_form': user_form
+    }
+
+    return render(request, 'sign_up.html', context=context_dict)
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    # Check not already logged in
+    if request.user.is_authenticated:
+        return redirect(reverse('na:index'))
+
+    # Check if submitting or loading
+    if request.method == 'POST':
+        # Validate submission
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Try find user
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            # TODO: check is_active
+            auth.login(request, user)
+            return redirect(reverse('na:index'))
+        else:
+            error = "Invalid login details." 
+    else:
+        # Setup empty form
+        error = None
+
+    context_dict = {
+        'error': error,
+    }
+
+    return render(request, 'login.html', context=context_dict)    
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('na:index'))
