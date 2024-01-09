b011bed54a6db2505ef3e25fc69a4c83a1288dee
andrei <andrei.ghita2002@gmail.com>
2024-01-09 17:57:42 +0000

    reactapp: Made an example component that fetches data from an API, exposed by python code.

--- neuro_arcade/reactapp/src/App.js
+++ neuro_arcade/reactapp/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const api_url = '/react_test/api/';
+
+    const [data, setData] = useState('nothing');
+
+    const options_body = {
+        'skip': '1',
+        'numbers:': '12',
+    };
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(options_body),
+    };
+
+    useEffect(() => {
+        fetch(api_url, options)
+            .then(response => {
+                if (response.ok) {
+                    return response.json(); // parse JSON data
+                } else {
+                    throw new Error('Response was not ok');
+                }
+            })
+            .then(jsonData => {
+                setData(JSON.stringify(jsonData, null, 2));
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                setData('Error occurred!');
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <p>hi!</p>
+            <p>response: {data}</p>
+        </div>
+    );
 }
 
 export default App;
--- neuro_arcade/reactapp/urls.py
+++ neuro_arcade/reactapp/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'reactapp'
 
 urlpatterns = [
-    path('react_test', views.react_test, name='react_index'),
+    path('react_test/', views.react_test, name='react_index'),
+    path('react_test/api/', views.fibbonaci, name='api_test')
 ]
