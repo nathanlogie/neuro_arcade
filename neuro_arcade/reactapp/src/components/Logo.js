3d53c79013ed54869039d4c32ba61d359e83509a
Nour Sameh Mohamed Hosni Elfangary <2660369s@student.gla.ac.uk>
2024-01-13 14:44:25 +0000

    Added neuroarcade background started work on Banner

---
+++ neuro_arcade/reactapp/src/components/Logo.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Logo.module.css"
+
+export function Logo() {
+    return  (
+        <div>
+            <img src="../../../static/images/logo.png" alt=""/> //TODO DJANGO API
+        </div>
+    );
+}
