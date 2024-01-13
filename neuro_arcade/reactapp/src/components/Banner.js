24e6045855940654d386090f13718f4fa8ed4069
Nour Sameh Mohamed Hosni Elfangary <2660369s@student.gla.ac.uk>
2024-01-13 15:59:55 +0000

    Added responsiveness to Banner Created Index.js

--- neuro_arcade/reactapp/src/components/Banner.js
+++ neuro_arcade/reactapp/src/components/Banner.js
@@ -1,10 +1,26 @@
 import styles from '../styles/Banner.module.css';
+import {Button} from "./Button";
+import {Logo} from "./Logo";
 
 export function Banner({size}) {
     return (
         <div
             className={styles.Banner}
             id={styles[size]}
-        />
+        >
+            <Button
+                name={'About'}
+                link={'...'}
+                direction={'left'}
+                orientation={'left'}
+            />
+            <Logo size={'big'}/>
+            <Button
+                name={'Add Content'}
+                link={'hh'}
+                direction={'right'}
+                orientation={'right'}
+            />
+        </div>
     );
 }
