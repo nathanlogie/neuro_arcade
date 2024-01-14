41cdf702d9fb28193b97cfdf2e93669fd6372062
Nour Sameh Mohamed Hosni Elfangary <2660369s@student.gla.ac.uk>
2024-01-14 15:41:24 +0000

    Added fonts Made Banner modular

--- neuro_arcade/reactapp/src/components/Banner.js
+++ neuro_arcade/reactapp/src/components/Banner.js
@@ -2,25 +2,66 @@ import styles from '../styles/Banner.module.css';
 import {Button} from "./Button";
 import {Logo} from "./Logo";
 
-export function Banner({size}) {
-    return (
-        <div
-            className={styles.Banner}
-            id={styles[size]}
-        >
-            <Button
-                name={'About'}
-                link={'...'}
-                direction={'left'}
-                orientation={'left'}
-            />
-            <Logo size={'big'}/>
-            <Button
-                name={'Add Content'}
-                link={'hh'}
-                direction={'right'}
-                orientation={'right'}
-            />
-        </div>
-    );
+export function Banner({size, button_left, button_right}) {
+    if (size === 'big' && button_right !== null && button_left !== null) {
+        return (
+            <div
+                className={styles.Banner}
+                id={styles[size]}
+            >
+                <Button
+                    name={button_left.name}
+                    link={button_left.link}
+                    direction={button_left.direction}
+                    orientation={button_left.orientation}
+                />
+                <Logo size={size}/>
+                <Button
+                    name={button_right.name}
+                    link={button_right.link}
+                    direction={button_right.direction}
+                    orientation={button_right.orientation}
+                />
+            </div>
+        );
+    } else if (size === 'big' && button_right !== null) {
+        return (
+            <div
+                className={styles.Banner}
+                id={styles[size]}
+            >
+                <Logo size={size}/>
+                <Button
+                    name={button_right.name}
+                    link={button_right.link}
+                    direction={button_right.direction}
+                    orientation={button_right.orientation}
+                />
+            </div>
+        );
+    } else if (size === 'big' && button_left !== null) {
+        return (
+            <div
+                className={styles.Banner}
+                id={styles[size]}
+            >
+                <Button
+                    name={button_left.name}
+                    link={button_left.link}
+                    direction={button_left.direction}
+                    orientation={button_left.orientation}
+                />
+                <Logo size={size}/>
+            </div>
+        );
+    } else {
+        return (
+            <div
+                className={styles.Banner}
+                id={styles[size]}
+            >
+                <Logo size={size}/>
+            </div>
+        );
+    }
 }
