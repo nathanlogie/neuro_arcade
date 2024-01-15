8bc78925af4ca1ce256e0f06781e200806a24aae
Nathan George Logie <2658532l@student.gla.ac.uk>
2024-01-15 22:21:37 +0000

    Model tests for the GameTag model

--- neuro_arcade/na/tests.py
+++ neuro_arcade/na/tests.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from django.test import TestCase
-from na.models import Game
+from models import Game, GameTag
 # Create your tests here.
 
 
 class ModelTests(TestCase):
-    def createGame(self) -> Game:
+    def create_game(self) -> Game:
         return Game.objects.create(
             name="TestGame",
             description="This is a test description",
@@ -19,10 +19,22 @@ class ModelTests(TestCase):
         )
 
     def test_game(self):
-        testGame = self.createGame()
+        testGame = self.create_game()
         self.assertTrue(isinstance(testGame, Game))
         self.assertEquals(testGame.slug, slugify(testGame.name))
         self.assertEquals(str(testGame), testGame.name)
         self.assertTrue(isinstance(testGame.owner,User))
         self.assertEquals(testGame.score_type, {"headers": []})
 
+    def create_gameTag(self):
+        return GameTag.objects.create(
+            name="TestGameTag",
+            description="WOW this game has tags"
+        )
+
+    def test_gameTag(self):
+        testTag = self.create_gameTag()
+        self.assertTrue(isinstance(testTag,GameTag))
+        self.assertEquals(testTag.slug, slugify(testTag.name))
+        self.assertEquals(testTag.name, str(testTag))
+        self.assertEquals(testTag.name, "TestGameTag")
