8902b1fa034f209aa1cc3750d73c487159178303
Nathan George Logie <2658532l@student.gla.ac.uk>
2024-01-15 22:16:32 +0000

    Model tests for the Game model

--- neuro_arcade/na/tests.py
+++ neuro_arcade/na/tests.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 from django.test import TestCase
-
+from na.models import Game
 # Create your tests here.
+
+
+class ModelTests(TestCase):
+    def createGame(self) -> Game:
+        return Game.objects.create(
+            name="TestGame",
+            description="This is a test description",
+            owner=User.objects.create_user(
+                username="TestGameOwner",
+                password="SuperSecurePassword",
+                email="Tester@testing.com"
+            ),
+            play_link="www.l"
+        )
+
+    def test_game(self):
+        testGame = self.createGame()
+        self.assertTrue(isinstance(testGame, Game))
+        self.assertEquals(testGame.slug, slugify(testGame.name))
+        self.assertEquals(str(testGame), testGame.name)
+        self.assertTrue(isinstance(testGame.owner,User))
+        self.assertEquals(testGame.score_type, {"headers": []})
+
