9bda8cdd19ec7f6c499d5211865a9e9972dc2713
Eilidh <2660212m@student.gla.ac.uk>
2024-01-16 12:12:41 +0000

    table and final graph/switcher fixes

--- neuro_arcade/reactapp/src/components/Switcher.js
+++ neuro_arcade/reactapp/src/components/Switcher.js
@@ -2,16 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import styles from '../styles/Switcher.module.css'
+// import styles from '../styles/Switcher.module.css'
 
 
 const Switcher = ({data , onSwitcherChange}) => { 
 
     const [alignment, setAlignment] = React.useState('web');
-
-    // const handleChange = (event, newAlignment) => {
-    //     setAlignment(newAlignment);
-    // };
 
     const handleAlignment = (event, newAlignment) => {
         if (newAlignment !== null) {
@@ -21,18 +17,30 @@ const Switcher = ({data , onSwitcherChange}) => {
     };
 
     return(
-        <div className={styles.Switcher}>
+        <div className="switcher">
             <ToggleButtonGroup
                 color="primary"
                 value={alignment}
                 exclusive
                 onChange={handleAlignment}
                 aria-label="Platform"
+                style={{ 
+                    borderRadius: '40px',
+                    overflow: 'hidden',
+                }}
             >
 
                 {data.table_headers.map((header, index) => (   
                     <div key={index}>
-                        <ToggleButton value={header.name}>{header.name}</ToggleButton>
+                        <ToggleButton 
+                            value={header.name}
+                            style={{
+                                borderColor: 'transparent',
+                                backgroundColor: alignment === header.name ? 'gray' : 'transparent',
+                                color: 'black'
+                            }}>
+                                {header.name}
+                        </ToggleButton>
                     </div>
                 ))}
             </ToggleButtonGroup>
@@ -42,6 +50,7 @@ const Switcher = ({data , onSwitcherChange}) => {
 
 Switcher.propTypes = {
     inputData: PropTypes.object.isRequired,
+    onSwitcherChange: PropTypes.func.isRequired,
 }
 
 export default Switcher
