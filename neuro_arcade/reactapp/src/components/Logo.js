5e4d2fd7290164dd9a42ee783ef0185e2c3c5b43
andrei <andrei.ghita2002@gmail.com>
2024-01-17 11:11:53 +0000

    reactapp: removed a Todo in Logo

--- neuro_arcade/reactapp/src/components/Logo.js
+++ neuro_arcade/reactapp/src/components/Logo.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 export function Logo({size}) {
     return  (
         <Link className={styles.Logo} id={styles[size]} to={'/'}>
-            <img src={logo} alt="" onClick={'...'} /> {/*TODO Django link API*/}
+            <img src={logo} alt="Neuro Arcade" />
             <h1 id={styles['title_1']}>neuro</h1>
             <h1 id={styles['title_2']}>arcade</h1>
         </Link>
