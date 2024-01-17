b5a65c4f39d98a1f07b437756080267887e9c0ed
andrei <andrei.ghita2002@gmail.com>
2024-01-17 10:19:12 +0000

    reactapp: fixed GameView

--- neuro_arcade/reactapp/src/app/GameView.js
+++ neuro_arcade/reactapp/src/app/GameView.js
@@ -1,18 +1,34 @@
-import {useParams} from "react-router-dom";
-import {requestGame} from "../backendRequests";
+import {useLocation, useParams} from "react-router-dom";
+import {requestGame, requestGamesSorted} from "../backendRequests";
 import styles from "../styles/App.module.css";
 import {Table} from "../components/Table";
+import {useEffect, useState} from "react";
 
 
 export function GameView() {
     // see: https://reactrouter.com/en/main/start/concepts#data-access
-    let {game_slug} = useParams();
-    let game_data = requestGame(game_slug);
-
-    return (
-        <div className={styles.MainBlock}>
-            <p>GAME VIEW!!!</p>
-            <Table inputData={game_data}/>
-        </div>
-    )
+    let gameSlug = useParams().game_slug;
+    let [isLoading, setLoading] = useState(true);
+    let [gameData, setGameData] = useState({});
+    useEffect(() => {
+        requestGame(gameSlug)
+            .then(g => {
+                setGameData(g);
+                setLoading(false);
+            })
+    }, []);
+    if (isLoading) {
+        return (
+            <div className={styles.MainBlock}>
+                Loading...
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.MainBlock}>
+                <p>GAME VIEW for {gameData.game.name} !!!</p>
+                <Table inputData={gameData}/>
+            </div>
+        )
+    }
 }
