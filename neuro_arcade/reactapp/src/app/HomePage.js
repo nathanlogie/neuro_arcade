9c80abe90028c9f12ba573100ca21578f04b94b2
andrei <andrei.ghita2002@gmail.com>
2024-01-17 12:21:41 +0000

    API: get_games_sorted allows different number of games to be shown

--- neuro_arcade/reactapp/src/app/HomePage.js
+++ neuro_arcade/reactapp/src/app/HomePage.js
@@ -47,7 +47,7 @@ export function HomePage() {
             >
                 <div className={styles.Content}>
                     <h1>Featured games</h1>
-                    <GameGrid query={'?query=&tags=featured'} />
+                    <GameGrid query={'?query=&tags=featured&num=10'} />
                     <Button
                         id={'MoreGames'}
                         name={'more games'}
