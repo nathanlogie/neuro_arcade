b336944c5772f829863d0da17bb4705904bfde6a
andrei <andrei.ghita2002@gmail.com>
2024-01-17 11:07:47 +0000

    reactapp: basic AllGames.js

--- neuro_arcade/reactapp/src/app/AllGames.js
+++ neuro_arcade/reactapp/src/app/AllGames.js
@@ -1,12 +1,23 @@
 import {Banner} from "../components/Banner";
 import {Background} from "../components/Background";
 import styles from "../styles/App.module.css"
+import {GameGrid} from "../components/GameGrid";
+import {useState} from "react";
 
 export function AllGames() {
+    let [query, setQuery] = useState('');
+    //todo add a search bar
     return (
         <div>
-            <Background />
-            <Banner size={'small'} />
+            <Background/>
+            <Banner size={'small'}/>
+            <div className={styles.Side}>
+                {/* search bar + tags here */}
+            </div>
+            <div className={styles.Content}>
+                <h1>Games:</h1>
+                <GameGrid query={query} />
+            </div>
         </div>
     );
 }
