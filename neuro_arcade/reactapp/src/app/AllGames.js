88fa50126630bab2cc249faa223031c3ebf3973f
Daniel Hally <2660097h@student.gla.ac.uk>
2024-01-29 22:06:32 +0000

    GameGrid tagQuery parameter
    Allows for a GameGrid's displayed games to be filtered by a list
    of required tags

--- neuro_arcade/reactapp/src/app/AllGames.js
+++ neuro_arcade/reactapp/src/app/AllGames.js
@@ -31,7 +31,7 @@ export function AllGames() {
                 </div>
                 <div className={styles.Content} id={styles['AllGames']}>
                     <h1>All Games</h1>
-                    <GameGrid query={''} nameQuery={nameQuery} linkPrefix={''} id={'AppGrid'}/>
+                    <GameGrid query={''} nameQuery={nameQuery} tagQuery={selectedTags} linkPrefix={''} id={'AppGrid'}/>
                 </div>
             </div>
         </div>
