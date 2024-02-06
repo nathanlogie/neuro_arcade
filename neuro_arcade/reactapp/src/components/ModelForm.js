d889c81c14b29f26294029a47fd1168e07b7ea5a
Nathan George Logie <2658532l@student.gla.ac.uk>
2024-02-06 21:34:54 +0000

    Name Submission

--- neuro_arcade/reactapp/src/components/ModelForm.js
+++ neuro_arcade/reactapp/src/components/ModelForm.js
@@ -2,6 +2,7 @@ import {useForm} from "react-hook-form";
 import React, {useState} from "react";
 import axios from "axios";
 
+let MAXNAMELENGTH = 64
 export const ModelForm = () => {
     const {
         register,
@@ -15,5 +16,28 @@ export const ModelForm = () => {
     const [AIStatus, setAIStatus] = useState(false)
     const [description, setDescription] = useState("")
     const [tags, setTags] = useState("")
-    
+
+    const onSubmit = (event) => void {
+
+    }
+
+    return (
+        <form className={"form"} onSubmit={handleSubmit(onSubmit)}>
+            <h3>Name</h3>
+            <input {...register("name", {
+                required: "Name is required",
+                maxLength: {
+                    value: MAXNAMELENGTH,
+                    message: `Maximum name length has been exceeded (${MAXNAMELENGTH})`
+                }
+            })}
+                type={"text"} placeholder={"Name!"}
+            />
+            {errors.name && (
+                <div className={"errorMessage"}>{errors.name.message}</div>
+            )}
+
+
+        </form>
+    )
 };
