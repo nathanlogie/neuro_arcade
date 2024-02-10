80d5f95e421fb6613c4022a6c995c94714f40d81
Eilidh <2660212m@student.gla.ac.uk>
2024-02-10 13:55:00 +0000

    Graphs: more accuracte display of data

--- neuro_arcade/reactapp/src/components/game/RadarC.js
+++ neuro_arcade/reactapp/src/components/game/RadarC.js
@@ -18,22 +18,22 @@ export function RadarC() {
   // const handleSwitcherChange = (selectedValue) => {
   //     setSelectedSwitcherValue(selectedValue);
 
-  
-  const data = [
-    {player_name: 'Tiger Bot', coins: 84, points: 23},
-    {player_name: 'Amanda Wilson', coins: 72, points: 17},
-    {player_name: 'Monkey Bot', coins: 48, points: 53},
-    {player_name: 'Jenny Bar', coins: 56, points: 65},
-    {player_name: 'Panda Bot', coins: 25, points: 48},
-  ];
+
+  const data =[
+    {data_type: "Coins", average_ai: 23, average_human: 74},
+    {data_type: "Points", average_ai: 82, average_human: 42},
+    {data_type: "Stars", average_ai: 92, average_human: 27},
+    {data_type: "Time", average_ai: 45, average_human: 86},
+    {data_type: "Wins", average_ai: 74, average_human: 36},
+  ]
 
   return (
     <RadarChart cx={300} cy={250} outerRadius={150} width={600} height={500} data={data}>
       <PolarGrid />
-      <PolarAngleAxis dataKey="player_name" />
+      <PolarAngleAxis dataKey="data_type" />
       <PolarRadiusAxis />
-      <Radar name="Coins" dataKey="coins" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-      <Radar name="Points" dataKey="points" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
+      <Radar name="Average AI Score" dataKey="average_ai" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+      <Radar name="Average Human Score" dataKey="average_human" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
       <Legend />
     </RadarChart>
   );
