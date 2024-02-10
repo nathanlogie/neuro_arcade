9a5697945d41660d769b4c6d2c6eb7ceb4f18732
Eilidh <2660212m@student.gla.ac.uk>
2024-02-10 19:26:02 +0000

    Graphs: Radar Chart data mapping and documentation

--- neuro_arcade/reactapp/src/components/game/RadarC.js
+++ neuro_arcade/reactapp/src/components/game/RadarC.js
@@ -3,29 +3,54 @@ import PropTypes from 'prop-types'
 import {Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis} from 'recharts';
 
 /**
- * 
+ * @param inputData {Object}
  * @returns {Element}
+ * takes in the input data from requestGame to build a Radar Chart
  */
-export function RadarC() {
-  // // setting the default switcher value to the first score
-  // const [selectedSwitcherValue, setSelectedSwitcherValue] = React.useState(
-  //     inputData.table_headers[0].name
-  // );
-  // const data = inputData.rows.map((player) => ({
-  //     name: player.player_name,
-  //     value: player.score[inputData.table_headers.findIndex((h) => h.name === selectedSwitcherValue)],
-  // }));
-  // const handleSwitcherChange = (selectedValue) => {
-  //     setSelectedSwitcherValue(selectedValue);
+export function RadarC({inputData}) {
 
+  const scores = {};
+    /**
+     * @param header {Object}
+     * for each table header (score type) initialises the score object and sets the
+     * counts and totals of AI and humans to 0
+     */
+    inputData[0].table_headers.forEach(function(header){
+        scores[header.name] = { ai: { count: 0, total: 0 }, human: { count: 0, total: 0 } };
+    });
 
-  const data =[
-    {data_type: "Coins", average_ai: 23, average_human: 74},
-    {data_type: "Points", average_ai: 82, average_human: 42},
-    {data_type: "Stars", average_ai: 92, average_human: 27},
-    {data_type: "Time", average_ai: 45, average_human: 86},
-    {data_type: "Wins", average_ai: 74, average_human: 36},
-  ]
+    /**
+     * @param row {Object}
+     * for each player in row decides if the player is AI or human
+     */
+    inputData[0].rows.forEach(function(row){
+        const scoreType = row.is_ai ? 'ai' : 'human';
+        /**
+         * @param header {Object}
+         * @param index {integer}
+         * finds the total count of AI and human players and the total score for AI and human players
+         */
+        inputData[0].table_headers.forEach(function(header, index){
+            scores[header.name][scoreType].count++;
+            scores[header.name][scoreType].total += row.score[index];
+        });
+    });
+
+    const data = [];
+    /**
+     * @param header {Object}
+     * finds the average AI and human scores then adds this to an object data to be used in the Radar Chart
+     */
+    inputData[0].table_headers.forEach(function(header){
+        const averageAI = scores[header.name].ai.total / scores[header.name].ai.count;
+        const averageHuman = scores[header.name].human.total / scores[header.name].human.count;
+
+        data.push({
+            data_type: header.name,
+            average_ai: averageAI.toFixed(2),
+            average_human: averageHuman.toFixed(2)
+        });
+    });
 
   return (
     <RadarChart cx={300} cy={250} outerRadius={150} width={600} height={500} data={data}>
@@ -39,3 +64,6 @@ export function RadarC() {
   );
 }
 
+RadarC.propTypes = {
+  inputData: PropTypes.object.isRequired,
+}
