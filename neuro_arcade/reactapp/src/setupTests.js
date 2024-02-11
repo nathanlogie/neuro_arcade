54c47d7b8ff18c403bb7dade6e8ab9136d62ea02
Daniel Hally <2660097h@student.gla.ac.uk>
2024-02-11 18:09:39 +0000

    React: component level tests
    Sets up component testing through react-testing-library.
    Creates a few example test cases for CardGrid.

---
+++ neuro_arcade/reactapp/src/setupTests.js
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+// Components with Links inside need to be wrapped in a router
+global.renderWithRouter = (html) => {
+    return render(
+        <MemoryRouter>
+            {html}
+        </MemoryRouter>
+    );
+}
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
