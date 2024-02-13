4a1b29fa18d01097dd885c9a373df2621e7e4dc3
Daniel Hally <2660097h@student.gla.ac.uk>
2024-02-13 22:41:54 +0000

    Logo: create tests
    Basic rendering check for big and small mode

---
+++ neuro_arcade/reactapp/src/components/__tests__/Logo.js
@@ -0,0 +1,14 @@
+import { screen } from "@testing-library/react"
+import { Logo } from "../Logo";
+
+test('Logo renders without crashing in big mode', () => {
+    renderWithRouter(<Logo size='big' />);
+
+    expect(screen.getByAltText("Neuro Arcade")).toBeInTheDocument();
+});
+
+test('Logo renders without crashing in small mode', () => {
+    renderWithRouter(<Logo size='small' />);
+
+    expect(screen.getByAltText("Neuro Arcade")).toBeInTheDocument();
+});
