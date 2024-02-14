6dcd4c1dab10fa0d1a3ac1300fdf42ee83fd04e9
Daniel Hally <2660097h@student.gla.ac.uk>
2024-02-14 10:34:35 +0000

    Button: test link functionality
    Adds a userEvent test for clicking a button to ensure it takes
    the user to the requested destination

--- neuro_arcade/reactapp/src/components/__tests__/Button.js
+++ neuro_arcade/reactapp/src/components/__tests__/Button.js
@@ -1,5 +1,7 @@
 import { screen } from "@testing-library/react";
 import { Button } from "../Button";
+import { Route } from "react-router";
+import userEvent from "@testing-library/user-event";
 
 // TODO: currently the case where link is an empty string is not tested
 // This seems like it should be removed, but if not then tests should be made
@@ -80,6 +82,23 @@ test('Button renders right orientation without crashing', () => {
     );
 
     expect(screen.getByText("Name")).toBeInTheDocument();
+});
+
+test('Button link functions', () => {
+    renderWithRouter(
+        <Button
+            name='Name'
+            link='dest'
+            direction='left'
+            orentation='left'
+        />,
+
+        <Route path="dest" element={<p>Success</p>} />
+    );
+
+    userEvent.click(screen.getByText("Name"));
+
+    expect(screen.getByText("Success")).toBeInTheDocument();
 });
 
 // TODO: test link?
