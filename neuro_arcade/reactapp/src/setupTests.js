d2ef00abfb8bae35bd8b3d71abc2267dd5a81102
Daniel Hally <2660097h@student.gla.ac.uk>
2024-02-14 10:13:11 +0000

    React: setup for link testing
    Allows extra routes to be added in renderWithRouter, and updates testing
    library to fix some false warnings that were printed when using userEvent
    on Link components

--- neuro_arcade/reactapp/src/setupTests.js
+++ neuro_arcade/reactapp/src/setupTests.js
@@ -2,11 +2,12 @@ import { render } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 // Components with Links inside need to be wrapped in a router
-global.renderWithRouter = (html) => {
+global.renderWithRouter = (html, extra) => {
     return render(
         <MemoryRouter>
             <Routes>
                 <Route path="" element={html} />
+                {extra || ""}
             </Routes>
         </MemoryRouter>
     );
