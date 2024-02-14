bee495e3aee4a19a51aa6ba592c5b7b7bd26839e
Nathan George Logie <2658532l@student.gla.ac.uk>
2024-02-14 21:35:14 +0000

    Linter - Python Linted

--- neuro_arcade/na/tests.py
+++ neuro_arcade/na/tests.py
@@ -5,7 +5,6 @@ from na.models import Game, GameTag, Player, PlayerTag, Score
 
 # The ide will shout at you to remove the na from the import but don't
 # The na allows it to run in terminal and in the pipeline
-from na.models import Game, GameTag, Player, PlayerTag, Score
 import populate
 
 # Create your tests here.
