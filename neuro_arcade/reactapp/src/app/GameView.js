85c4062c84154135690634af30eda14d41c23a96
Nour Sameh Mohamed Hosni Elfangary <2660369s@student.gla.ac.uk>
2024-02-23 23:48:03 +0000

    Animated GameView

--- neuro_arcade/reactapp/src/app/GameView.js
+++ neuro_arcade/reactapp/src/app/GameView.js
@@ -7,6 +7,7 @@ import {SwarmPlot} from "../components/game/SwarmPlot";
 import React, {useEffect, useState} from "react";
 import {Banner, MobileBanner} from "../components/Banner";
 import {Switcher} from "../components/Switcher";
+import {motion} from "framer-motion";
 
 /**
  *
@@ -45,49 +46,54 @@ export function GameView() {
 
     let content = <>...</>;
     if (!loading) {
-        content = <>
-            <div className={styles.Content}>
-                <div className={styles.Title}>
-                    <h1>{gameData.game.name}</h1>
-                </div>
-                <div className={styles.ContentBlock}>
-                    <p>
-                        <img src="https://loremflickr.com/500/500" alt={'image'} // TODO add query for image here
-                        />
-                        {gameData.game.description}
-                    </p>
-                </div>
-            </div>
-            <div className={styles.DataBlock}>
-                <Table inputData={gameData}/>
-                <div className={styles.Graphs}>
-                    <h2>Trends</h2>
-                    <div className={styles.GraphSwitcher}>
-                        {gameData.table_headers.length > 2 ?
-                            <Switcher
-                                data={graphHeaders}
-                                onSwitcherChange={handleSwitcherChange}
-                                switcherDefault={selectedSwitcherValue}
-                                id={styles['vertical']}
-                            /> : <></>
-                        }
+        content =
+            <motion.div
+                className={styles.MainBlock}
+                id={styles['small']}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+            >
+                <div className={styles.Content}>
+                    <div className={styles.Title}>
+                        <h1>{gameData.game.name}</h1>
                     </div>
-                    <div className={styles.Background}>
-                        {selectedSwitcherValue === 'Swarm Plot' ? <SwarmPlot inputData={gameData}/> : <></>}
-                        {selectedSwitcherValue === 'Radar Chart' ? <RadarC inputData={gameData}/> : <></>}
+                    <div className={styles.ContentBlock}>
+                        <p>
+                            <img src="https://loremflickr.com/500/500" alt={'image'} // TODO add query for image here
+                            />
+                            {gameData.game.description}
+                        </p>
                     </div>
                 </div>
-            </div>
-        </>;
+                <div className={styles.DataBlock}>
+                    <Table inputData={gameData}/>
+                    <div className={styles.Graphs}>
+                        <h2>Trends</h2>
+                        <div className={styles.GraphSwitcher}>
+                            {gameData.table_headers.length > 2 ?
+                                <Switcher
+                                    data={graphHeaders}
+                                    onSwitcherChange={handleSwitcherChange}
+                                    switcherDefault={selectedSwitcherValue}
+                                    id={styles['vertical']}
+                                /> : <></>
+                            }
+                        </div>
+                        <div className={styles.Background}>
+                            {selectedSwitcherValue === 'Swarm Plot' ? <SwarmPlot inputData={gameData}/> : <></>}
+                            {selectedSwitcherValue === 'Radar Chart' ? <RadarC inputData={gameData}/> : <></>}
+                        </div>
+                    </div>
+                </div>
+            </motion.div>;
     }
 
     return (
         <>
-            <Banner size={'small'} selected={'Games'} />
-                <MobileBanner/>
-                <div className={styles.MainBlock} id={styles['small']}>
-                    {content}
-                </div>
-            </>
-        );
+            <Banner size={'small'} selected={'Games'}/>
+            <MobileBanner/>
+            {content}
+        </>
+    );
 }
