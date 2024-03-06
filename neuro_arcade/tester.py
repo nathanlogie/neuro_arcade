127a4547651def493424a4bba2fe8bd15dd93e7a
Nathan George Logie <2658532l@student.gla.ac.uk>
2024-03-06 17:48:42 +0000

    Email: Working Version

--- neuro_arcade/tester.py
+++ neuro_arcade/tester.py
@@ -1,49 +1,18 @@
-# #
-# # from django.conf import settings
-# # from django.core.mail import send_mail
-# #
-# # settings.configure()
-# #
-# # port = 587  # For starttls
-# # smtp_server = "smtp.gmail.com"
-# # sender_email = "arcadeneuro@gmail.com"
-# # receiver_email = ""
-# # password = ""
-# # message = """\
-# # Subject: Hi there
-# #
-# # This message is sent from Python."""
-# #
-# # send_mail(
-# #     subject='A cool subject',
-# #     message=message,
-# #     from_email=settings.EMAIL_HOST_USER,
-# #     recipient_list=[receiver_email],
-# #     fail_silently=False
-# # )
-# from django.conf import settings
-# from django.core.mail import send_mail
-# from django.template.loader import render_to_string
-# settings.configure()
-#
-# def sendConfirmEmail(email, code):
-#     mail_subject = 'Confirmation code {}'.format(code)
-#     message = "Hello"
-#     to_email = email
-#     send_mail(mail_subject, message, 'arcadeneuro@gmail.com', [to_email],
-#               fail_silently=False)
-#
-#
-# sendConfirmEmail('nathanlogie20@gmail.com', 1234)
+# Must run with sudo via commandline
 
+import os
+import sys
+
+import django
 from django.conf import settings
 from django.core.mail import send_mail
 
-settings.configure()
+sys.path.insert(0, '../neuro_arcade')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neuro_arcade.settings')
+django.setup()
 
-subject = 'welcome to GFG world'
-message = f'Hi, thank you for registering in geeksforgeeks.'
+subject = 'Neuro Arcade'
+message = 'Hi, Developers!!!!.'
 email_from = settings.EMAIL_HOST_USER
-recipient_list = []
-send_mail(subject, message, email_from, recipient_list )
-
+recipient_list = [""]
+send_mail(subject, message, email_from, recipient_list)
