d9e12c1ca7e5006e88612d8e173ceb2541ba427c
Nour Sameh Mohamed Hosni Elfangary <2660369s@student.gla.ac.uk>
2024-03-13 01:53:13 +0000

    Styled AllUsers

--- neuro_arcade/reactapp/src/app/user_account/AllUsers.js
+++ neuro_arcade/reactapp/src/app/user_account/AllUsers.js
@@ -3,16 +3,25 @@ import {MobileBanner} from "../../components/Banner";
 import {motion} from "framer-motion";
 import {getAllUsers, updateStatus} from "../../backendRequests";
 import React, {useEffect, useState} from "react";
-import styles from "../../styles/components/Table.module.css";
+import styles from "../../styles/App.module.css";
+import table from "../../styles/components/Table.module.css";
 import {createTheme, ThemeProvider} from "@mui/material";
 import {DataGrid} from "@mui/x-data-grid";
+import {NavBar} from "../../components/NavBar";
+import {Button} from "../../components/Button";
+import {FaPlus} from "react-icons/fa6";
+import {MdBlock} from "react-icons/md";
+import {ImCross} from "react-icons/im";
 
 /**
  * Page for admins only
  * Lets admins see all users and approve new ones
  */
 export function AllUsers() {
+
     const [users, setUsers] = useState([]);
+
+    let nav_left = <Button name={"user account"} link={"/user_account"} orientation={"left"} direction={"left"} />;
 
     useEffect(() => {
         getAllUsers().then((usersResponse) => {
@@ -36,26 +45,54 @@ export function AllUsers() {
 
     const columns = [
         {field: "username", width: 150, renderHeader: () => <strong>Username</strong>},
-        {field: "email", width: 150, renderHeader: () => <strong>Email</strong>},
+        {field: "email", width: 175, renderHeader: () => <strong>Email</strong>},
         {field: "status", width: 150, renderHeader: () => <strong>Status</strong>},
         {
             field: "actions",
-            headerName: "Actions",
-            width: 200,
+            width: 250,
+            renderHeader: () => <strong>Actions</strong>,
             renderCell: (params) => {
                 const user = params.row;
                 if (user.status === "approved") {
                     return (
                         <>
-                            <button onClick={() => changeUserStatus(user, "pending")}>Revoke Approval</button>
-                            <button onClick={() => changeUserStatus(user, "blocked")}>Block User</button>
+                            <motion.button className={table.Revoke} onClick={() => changeUserStatus(user, "pending")}
+                                           whileHover={{scale: 1.1}}
+                                           whileTap={{scale: 0.9}}>
+                                Revoke
+                                <div>
+                                    <ImCross/>
+                                </div>
+                            </motion.button>
+                            <motion.button className={table.Block} onClick={() => changeUserStatus(user, "blocked")}
+                                           whileHover={{scale: 1.1}}
+                                           whileTap={{scale: 0.9}}>
+                                Block
+                                <div>
+                                    <MdBlock/>
+                                </div>
+                            </motion.button>
                         </>
                     );
                 } else if (user.status === "pending") {
                     return (
                         <>
-                            <button onClick={() => changeUserStatus(user, "approved")}>Approve User</button>
-                            <button onClick={() => changeUserStatus(user, "blocked")}>Block User</button>
+                            <motion.button className={table.Approve} onClick={() => changeUserStatus(user, "approved")}
+                                           whileHover={{scale: 1.1}}
+                                           whileTap={{scale: 0.9}}>
+                                Approve
+                                <div>
+                                    <FaPlus/>
+                                </div>
+                            </motion.button>
+                            <motion.button className={table.Block} onClick={() => changeUserStatus(user, "blocked")}
+                                           whileHover={{scale: 1.1}}
+                                           whileTap={{scale: 0.9}}>
+                                Block
+                                <div>
+                                    <MdBlock/>
+                                </div>
+                            </motion.button>
                         </>
                     );
                 } else if (user.status === "blocked") {
@@ -82,9 +119,9 @@ export function AllUsers() {
 
     return (
         <>
-            <Banner size={"big"} />
+            <Banner size={"big"} left={nav_left} />
             <MobileBanner size={"big"} />
-
+            <NavBar left={nav_left} />
             <motion.div
                 className={styles.MainBlock}
                 id={styles["big"]}
@@ -92,11 +129,9 @@ export function AllUsers() {
                 animate={{opacity: 1, x: 0}}
                 exit={{opacity: 0, x: 100}}
             >
-                <div className={styles.Content} id={styles["small"]}>
-                    <div className={styles.Title}>
-                        <h1>ALL USERS</h1>
-                    </div>
-                    <div className={styles.ContentBlock}>
+                <div className={styles.Content} id={styles["big"]}>
+                    <div className={table.TableContainer} id={table["allUsers"]}>
+                        <h2>All Users</h2>
                         <ThemeProvider theme={table_theme}>
                             <DataGrid
                                 sx={{
@@ -108,7 +143,7 @@ export function AllUsers() {
                                         color: "white"
                                     },
                                     height: "100%",
-                                    width: "50%"
+                                    width: "100%"
                                 }}
                                 rows={rows}
                                 columns={columns}
@@ -117,8 +152,8 @@ export function AllUsers() {
                         </ThemeProvider>
                     </div>
                 </div>
+                <div className={styles.MobileBannerBuffer}/>
             </motion.div>
-            <div className={styles.MobileBannerBuffer} />
         </>
     );
 }
