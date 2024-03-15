fdb68a85470ea6234d7610b13b7eb8c427df4e1b
andrei <andrei.ghita2002@gmail.com>
2024-03-15 04:09:04 +0000

    setup script: installing docker

--- setup.sh
+++ setup.sh
@@ -26,6 +26,15 @@ cd ./neuro_arcade/reactapp/
 npm install
 cd ../../
 
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+# install docker
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # creating the docker image for evaluating scripts
 cd ./evaluation/
 sh docker_build.sh
