d590c07da8a92caf1a0a004a403b02789c6fc717
andrei <andrei.ghita2002@gmail.com>
2024-03-15 03:49:42 +0000

    built setup and run deployment scripts

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+PYTHON='python3.9'
+
+# run DB process
+sh ./db/run.sh
+
+# django server
+screen -dmS django_server $PYTHON neuro_arcade/manage.py runserver
+
+# frontend server
+# shellcheck disable=SC2164
+screen -dmS frontend_server bash -c "cd ./neuro_arcade/reactapp ; npm run build ; serve -s build"
--- setup.sh
+++ setup.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+apt-get update && upgrade -y
+
+# database
+sh ./db/init.sh
+
+# python
+apt-get install python3
+python -m ensurepip --upgrade
+
+# npm
+apt-get install nodejs
+
+# ngix
+apt-get install nginx
+cp nginx-config.txt /etc/nginx/sites-available/default
+nginx -t
+service nginx restart
+
+#start
+sh ./run_server.sh
