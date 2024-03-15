e351360e9d450818c2ad292e4512878602bc6803
Daniel Hally <2660097h@student.gla.ac.uk>
2024-03-15 16:41:47 +0000

    Evaluation: run in production
    Adds eval_runner into run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -13,3 +13,6 @@ screen -dmS django_server $PYTHON ./neuro_arcade/manage.py runserver
 
 # compiling the reactapp
 screen -dmS frontend_server bash -c "cd ./neuro_arcade/reactapp ; npm run build "
+
+# evaluation pipeline
+screen -dmS evaluation_server bash -c "cd ./evaluation ; $PYTHON eval_runner.py 4"
