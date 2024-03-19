27b47d33ebbf71558a8c4f4d245f51e7561c2ff6
tariq <2665724t@student.gla.ac.uk>
2024-03-19 15:07:04 +0000

    Styled 404 page

--- neuro_arcade/reactapp/src/app/PageNotFound.js
+++ neuro_arcade/reactapp/src/app/PageNotFound.js
@@ -1,26 +1,30 @@
-import {Link} from "react-router-dom";
 import styles from "../styles/App.module.css";
-import {motion} from "framer-motion";
+import {Button} from "../components/Button"
+import logo from "../static/images/logo.png"
 
 export function PageNotFound() {
     return (
-        <>
-            <div className={styles.Title}>
-                <h1>404 Page Not Found</h1>
-            </div>
-            <div className={styles.Content}>
-                <div className={styles.ContentBlock}>
-                    <motion.div
-                        className={styles.MainBlock}
-                        id={styles["big"]}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                    >
-                        <Link to={"/"}>Back to Neuroarcade</Link>
-                    </motion.div>
-                </div>
-            </div>
-        </>
+        <div className={styles.PageNotFound}>
+            {/*<div className={styles.Title}>*/}
+            {/*    <h1>404 Page Not Found</h1>*/}
+            {/*</div>*/}
+            {/*<div className={styles.Content}>*/}
+            {/*    <div className={styles.ContentBlock}>*/}
+            {/*        <motion.div*/}
+            {/*            className={styles.MainBlock}*/}
+            {/*            id={styles["big"]}*/}
+            {/*            initial={{opacity: 0}}*/}
+            {/*            animate={{opacity: 1}}*/}
+            {/*            exit={{opacity: 0}}*/}
+            {/*        >*/}
+            {/*            <Link to={"/"}>Back to Neuroarcade</Link>*/}
+            {/*        </motion.div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <img src={logo} alt={""}/>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <Button name={"Back to Neuroarcade"} link={"/"} orientation={"left"} direction={"left"} />
+        </div>
     );
 }
