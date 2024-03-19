31733dc243c4623f44195b9df564ef1577f7e78a
tariq <2665724t@student.gla.ac.uk>
2024-03-19 15:07:26 +0000

    Styled 404 page

--- neuro_arcade/reactapp/src/app/PageNotFound.js
+++ neuro_arcade/reactapp/src/app/PageNotFound.js
@@ -5,22 +5,6 @@ import logo from "../static/images/logo.png"
 export function PageNotFound() {
     return (
         <div className={styles.PageNotFound}>
-            {/*<div className={styles.Title}>*/}
-            {/*    <h1>404 Page Not Found</h1>*/}
-            {/*</div>*/}
-            {/*<div className={styles.Content}>*/}
-            {/*    <div className={styles.ContentBlock}>*/}
-            {/*        <motion.div*/}
-            {/*            className={styles.MainBlock}*/}
-            {/*            id={styles["big"]}*/}
-            {/*            initial={{opacity: 0}}*/}
-            {/*            animate={{opacity: 1}}*/}
-            {/*            exit={{opacity: 0}}*/}
-            {/*        >*/}
-            {/*            <Link to={"/"}>Back to Neuroarcade</Link>*/}
-            {/*        </motion.div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
             <img src={logo} alt={""}/>
             <h1>404</h1>
             <h2>Page Not Found</h2>
